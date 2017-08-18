ff58814aaf46fd9d203b95eae0d7c281bc838826
katiaash <katia.ashkar7@gmail.com>
2017-08-18 13:03:13 +0300

    adding nav bar for easy move between every section relate #15

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
+// Strart nav bar section
+var nav = document.querySelector(".nav"); // nav bar section
+var wrapper = document.querySelector(".wrapper");
+
+var navHeight = nav.offsetHeight;
+var topOfNav = nav.offsetTop;
+console.log(topOfNav);
+var windowHeight = window.innerHeight;
+
+var wrapperHeight = document.querySelector(".wrapper").offsetHeight;
+console.log(wrapperHeight);
+
+function fixNav() {
+  var currentScroll = window.scrollY;
+
+  if (currentScroll >= topOfNav) {
+    document.body.style.paddingTop = navHeight;
+    nav.classList.remove('bottom');
+    nav.classList.add('top');
+  } else {
+    nav.classList.remove('top');
+    nav.classList.add('bottom');
+  }
+  if (windowHeight - scrollY - navHeight <= wrapperHeight) {
+    wrapper.style.display = 'none';
+  } else {
+    wrapper.style.display = 'block';
+  }
+}
+
+window.addEventListener('scroll', fixNav);
+
+// End nav bar section
+
 var music = document.querySelector(".music");
-music.addEventListener("click", function (event) {
+music.addEventListener("click", function(event) {
   console.log('working');
   music.muted = true;
 })
 
 function checkMute() {
-    alert(music.muted);
+  alert(music.muted);
 }
 
 function toggleSound() {
   var elements = document.getElementsByTagName('audio');
-  for(var e = 0; e < elements.length; elements[e].muted = !elements[e].muted, e++);
-}
-
-
+  for (var e = 0; e < elements.length; elements[e].muted = !elements[e].muted, e++) ;
+  }
 
 //myAudio = new Audio('../songs/15.mp3');
 //if (typeof myAudio.loop == 'boolean')
 //{
-  //  myAudio.loop = true;
+//  myAudio.loop = true;
 //}
 //else
 //{
@@ -29,17 +61,16 @@ function toggleSound() {
 // myAudio.play();
 // }
 
-function checkusers()
-{
-   var shouldEnable = document.getElementById('checkbox').value == 0;
-   document.getElementById('add_button').disabled = shouldEnable;
+function checkusers() {
+  var shouldEnable = document.getElementById('checkbox').value == 0;
+  document.getElementById('add_button').disabled = shouldEnable;
 }
 
 var audio;
 
 //Hide Pause Initially
 var mute = document.getElementById("mute")
-mute.addEventListener("click", function(event){
+mute.addEventListener("click", function(event) {
   toggleSound();
   var play = document.getElementById("play")
   console.log(play);
@@ -50,103 +81,102 @@ mute.addEventListener("click", function(event){
 //Initializer - Play First Song
 initAudio($('#first'));
 
-function initAudio(element){
-    var song = element.attr('./songs/13.mp3');
-    var title = element.text('lama bdomik 3a sdere');
-    var cover = element.attr('cover');
-    var artist = element.attr('hussein al deek');
+function initAudio(element) {
+  var song = element.attr('./songs/13.mp3');
+  var title = element.text('lama bdomik 3a sdere');
+  var cover = element.attr('cover');
+  var artist = element.attr('hussein al deek');
 
-    //Create a New Audio Object
-    audio = new Audio('Music/' +'./songs/13.mp3');
+  //Create a New Audio Object
+  audio = new Audio('Music/' +
+    './songs/13.mp3');
 
-    if(!audio.currentTime){
-        $('#duration').html('0.00');
-    }
+  if (!audio.currentTime) {
+    $('#duration').html('0.00');
+  }
 
-    $('#audio-player .title').text(title);
-    $('#audio-player .artist').text(artist);
+  $('#audio-player .title').text(title);
+  $('#audio-player .artist').text(artist);
 
-    //Insert Cover Image
-    $('img.cover').attr('src','Pics/Cover/' + cover);
+  //Insert Cover Image
+  $('img.cover').attr('src', 'images/Cover/' + cover);
 
-    $('#playlist tr').removeClass('active');
-    element.parent('tr').addClass('active');
+  $('#playlist tr').removeClass('active');
+  element.parent('tr').addClass('active');
 }
 
-
 //Play Button
-$('#play').click(function(){
-    audio.play();
-    $('#play').hide();
-    $('#pause').show();
-    $('#duration').fadeIn(400);
-    showDuration();
+$('#play').click(function() {
+  audio.play();
+  $('#play').hide();
+  $('#pause').show();
+  $('#duration').fadeIn(400);
+  showDuration();
 });
 
 //Pause Button
-$('#pause').click(function(){
-    audio.pause();
-    $('#pause').hide();
-    $('#play').show();
+$('#pause').click(function() {
+  audio.pause();
+  $('#pause').hide();
+  $('#play').show();
 });
 
 //Stop Button
-$('#stop').click(function(){
-    audio.pause();
-    audio.currentTime = 0;
-    $('#pause').hide();
-    $('#play').show();
-    $('#duration').fadeOut(400);
+$('#stop').click(function() {
+  audio.pause();
+  audio.currentTime = 0;
+  $('#pause').hide();
+  $('#play').show();
+  $('#duration').fadeOut(400);
 });
 
 //Next Button
-$('#next').click(function(){
-    audio.pause();
-    var next = $('#playlist tr.active').next();
-    initAudio(next);
-    audio.play();
-    showDuration();
-    $('#play').hide();
-    $('#pause').show();
+$('#next').click(function() {
+  audio.pause();
+  var next = $('#playlist tr.active').next();
+  initAudio(next);
+  audio.play();
+  showDuration();
+  $('#play').hide();
+  $('#pause').show();
 
 });
 
 //Prev Button
-$('#prev').click(function(){
-    audio.pause();
-    var prev = $('#playlist tr.active').prev();
-    if (prev.length == 0) {
-        prev = $('#playlist tr:last-child');
-    }
-    initAudio(prev);
-    audio.play();
-    showDuration();
-    $('#play').hide();
-    $('#pause').show();
+$('#prev').click(function() {
+  audio.pause();
+  var prev = $('#playlist tr.active').prev();
+  if (prev.length == 0) {
+    prev = $('#playlist tr:last-child');
+  }
+  initAudio(prev);
+  audio.play();
+  showDuration();
+  $('#play').hide();
+  $('#pause').show();
 });
 
 //Playlist Song dblClick
-$('#playlist td:nth-child(2)').dblclick(function () {
-    audio.pause();
-    initAudio($(this, 'td:nth-child(2)'));
-    $('#play').hide();
-    $('#pause').show();
-    $('#duration').fadeIn(400);
-    audio.play();
-    showDuration();
+$('#playlist td:nth-child(2)').dblclick(function() {
+  audio.pause();
+  initAudio($(this, 'td:nth-child(2)'));
+  $('#play').hide();
+  $('#pause').show();
+  $('#duration').fadeIn(400);
+  audio.play();
+  showDuration();
 });
 //Playlist song click
 $('#playlist td:nth-child(2)').click(function() {
-    audio.pause();
-    $('#pause').hide();
-    $('#play').show();
+  audio.pause();
+  $('#pause').hide();
+  $('#play').show();
 });
 
 //Volume Control
-$('#volume').change(function(){
-   audio.volume = parseFloat(this.value / 10);
+$('#volume').change(function() {
+  audio.volume = parseFloat(this.value / 10);
 });
-
 
 //To Hide
 //{document.getElementById("p2").style.display="none";}
@@ -174,22 +204,19 @@ function doPost(e) {
     var mailData = e.parameters; // just create a slightly nicer variable name for the data
 
     MailApp.sendEmail({
-      to: TO_ADDRESS,
-      subject: "Contact form submitted",
+      to: TO_ADDRESS, subject: "Contact form submitted",
       // replyTo: String(mailData.email), // This is optional and reliant on your form actually collecting a field named `email`
       htmlBody: formatMailBody(mailData)
     });
 
-    return ContentService    // return json success results
-          .createTextOutput(
-            JSON.stringify({"result":"success",
-                            "data": JSON.stringify(e.parameters) }))
-          .setMimeType(ContentService.MimeType.JSON);
-  } catch(error) { // if error return this
+    return ContentService. // return json success results
+    createTextOutput(JSON.stringify({
+      "result": "success",
+      "data": JSON.stringify(e.parameters)
+    })).setMimeType(ContentService.MimeType.JSON);
+  } catch (error) { // if error return this
     Logger.log(error);
-    return ContentService
-          .createTextOutput(JSON.stringify({"result":"error", "error": e}))
-          .setMimeType(ContentService.MimeType.JSON);
+    return ContentService.createTextOutput(JSON.stringify({"result": "error", "error": e})).setMimeType(ContentService.MimeType.JSON);
   }
 }
 
@@ -198,29 +225,26 @@ function doPost(e) {
 function record_data(e) {
   Logger.log(JSON.stringify(e)); // log the POST data in case we need to debug it
   try {
-    var doc     = SpreadsheetApp.getActiveSpreadsheet();
-    var sheet   = doc.getSheetByName('responses'); // select the responses sheet
+    var doc = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = doc.getSheetByName('responses'); // select the responses sheet
     var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
-    var nextRow = sheet.getLastRow()+1; // get next row
-    var row     = [ new Date() ]; // first element in the row should always be a timestamp
+    var nextRow = sheet.getLastRow() + 1; // get next row
+    var row = [new Date()]; // first element in the row should always be a timestamp
     // loop through the header columns
     for (var i = 1; i < headers.length; i++) { // start at 1 to avoid Timestamp column
-      if(headers[i].length > 0) {
+      if (headers[i].length > 0) {
         row.push(e.parameter[headers[i]]); // add data to row
       }
     }
     // more efficient to set values as [][] array than individually
     sheet.getRange(nextRow, 1, 1, row.length).setValues([row]);
-  }
-  catch(error) {
+  } catch (error) {
     Logger.log(e);
-  }
-  finally {
+  } finally {
     return;
   }
 
 }
-
 
 const sendButton = document.querySelector('.button-success');
 const thankyouMessage = document.getElementById('thankyou_message');
